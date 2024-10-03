8b7b042f9fb2d388b09879979168fb4f7a3e2049
kanchan-karale067 <160843766+kanchan-karale067@users.noreply.github.com>
2024-10-03 21:30:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 var isDate = function (input) {
   //   write your code here
+	 if (input instanceof Date && !isNaN(input)) {
+    return true;
+  }
+
+  // Check if the input is a string or number that can be parsed into a valid date
+  if (typeof input === 'string' || typeof input === 'number') {
+    const parsedDate = new Date(input);
+    return !isNaN(parsedDate.getTime());
+  }
+
+  // For any other input type, return false
+  return false;
 };
 
 // Do not change the code below.
